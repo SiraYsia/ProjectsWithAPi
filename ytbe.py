f168b73878c6d1be016c41081cf0c83351766b2a
Yeabsira Tamiru <ytamiru@terpmail.umd.edu>
2023-06-22 15:59:44 +0000

    Delete config.py and modify ytbe.py

--- ytbe.py
+++ ytbe.py
@@ -1,15 +1,18 @@
+#Makes a request to the YouTube Data API to 
+#retrieve the most popular videos. It then 
+#extracts the titles of those videos and prints them.
+
 import requests
 import json
-import config
+import os
 
 url = 'https://www.googleapis.com/youtube/v3/videos'
-api_key = config.API_KEY
+api_key = os.environ.get('YOTUBE_API_KEY')
 
 params = {
     'part': 'snippet',
     'chart': 'mostPopular',
     'key': api_key
-    'key': 'AIzaSyDbkW3MTT4vkSL2XOGfezAwR_WJLVhV0O8' 
 }
 
 response = requests.get(url, params=params)

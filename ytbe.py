a1c47ed09d9fbea658db5543da88490df3cb2efd
Yeabsira Tamiru <ytamiru@terpmail.umd.edu>
2023-06-23 17:50:21 +0000

    Used SQL

--- ytbe.py
+++ ytbe.py
@@ -1,10 +1,11 @@
 #Makes a request to the YouTube Data API to 
 #retrieve the most popular videos. It then 
 #extracts the titles of those videos and prints them.
-
 import requests
-import json
+import pandas as pd
+import sqlalchemy as db
 import os
+
 
 url = 'https://www.googleapis.com/youtube/v3/videos'
 api_key = os.environ.get('YOTUBE_API_KEY')
@@ -17,12 +18,14 @@ params = {
 
 response = requests.get(url, params=params)
 data = response.json()
+video_titles = [item['snippet']['title'] for item in data['items']]
 
-if 'error' in data:
-    print(f"Error: {data['error']['message']}")
-else:
-    items = data.get('items', [])
-    for item in items:
-        snippet = item.get('snippet', {})
-        title = snippet.get('title')
-        print(title)
+
+data_frame = pd.DataFrame({'video_title': video_titles})
+engine = db.create_engine('sqlite:///data_base_name.db')
+data_frame.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+
+with engine.connect() as connection:
+    query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+    print(pd.DataFrame(query_result))

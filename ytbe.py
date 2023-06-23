ec77e4d40e4c717b3dea452c0ae05f2b40b10c9f
Yeabsira Tamiru <ytamiru@terpmail.umd.edu>
2023-06-23 19:11:40 +0000

    Add .github folder and check-style.yaml file

--- ytbe.py
+++ ytbe.py
@@ -1,14 +1,13 @@
-#Makes a request to the YouTube Data API to 
-#retrieve the most popular videos. It then 
-#extracts the titles of those videos and prints them.
+# Makes a request to the YouTube Data API to
+# retrieve the most popular videos. It then
+# extracts the titles of those videos and prints them.
 import requests
 import pandas as pd
 import sqlalchemy as db
 import os
 
-
 url = 'https://www.googleapis.com/youtube/v3/videos'
-api_key = os.environ.get('YOTUBE_API_KEY')
+api_key = os.environ.get('YOUTUBE_API_KEY')  # Fixed variable name
 
 params = {
     'part': 'snippet',
@@ -20,11 +19,9 @@ response = requests.get(url, params=params)
 data = response.json()
 video_titles = [item['snippet']['title'] for item in data['items']]
 
-
 data_frame = pd.DataFrame({'video_title': video_titles})
 engine = db.create_engine('sqlite:///data_base_name.db')
 data_frame.to_sql('table_name', con=engine, if_exists='replace', index=False)
-
 
 with engine.connect() as connection:
     query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()

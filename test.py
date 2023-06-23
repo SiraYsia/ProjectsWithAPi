fe67df297767a5b10775257db0f3c709e9881fe6
Yeabsira Tamiru <ytamiru@terpmail.umd.edu>
2023-06-23 20:19:30 +0000

    Add a test

---
+++ test.py
@@ -0,0 +1,33 @@
+import unittest
+import pandas as pd
+from ytbe import extract_video_titles, save_video_titles_tod_database, retrieve_from_database
+
+class TestYouTubeAPI(unittest.TestCase):
+    def test_extract_video_titles(self):
+        # Mock data for testing
+        data = {'items': [
+            {'snippet': {'title': 'Video 1'}},
+            {'snippet': {'title': 'Video 2'}},
+            {'snippet': {'title': 'Video 3'}}
+        ]}
+
+        # Test the extracting of video titles 
+        video_titles = extract_video_titles(data)
+        expected_titles = ['Video 1', 'Video 2', 'Video 3']
+        self.assertEqual(video_titles, expected_titles)
+
+    def test_retrieve_from_database(self):
+    # Prepare the test data in the database
+      db_name = 'test_db.db'
+      table_name = 'test_table'
+      video_titles = ['Title 1', 'Title 2', 'Title 3']
+      save_video_titles_tod_database(video_titles, db_name)
+    
+    # Test retrieving data from the database
+      retrieved_data = retrieve_from_database(db_name)
+      expected_data = pd.DataFrame({'video_title': video_titles})
+      self.assertTrue(retrieved_data.equals(expected_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- ytbe.py
+++ ytbe.py
@@ -1,30 +1,39 @@
-# Makes a request to the YouTube Data API to
-# retrieve the most popular videos. It then
-# extracts the titles of those videos and prints them.
+#Makes a request to the YouTube Data API to 
+#retrieve the most popular videos. It then 
+#extracts the titles of those videos and prints them.
 import requests
 import pandas as pd
 import sqlalchemy as db
 import os
 
-url = 'https://www.googleapis.com/youtube/v3/videos'
-api_key = os.environ.get('YOUTUBE_API_KEY')  # Fixed variable name
+def makee_youtube_api_requests():
+    url = 'https://www.googleapis.com/youtube/v3/videos'
+    api_key = os.environ.get('YOTUBE_API_KEY')
 
-params = {
-    'part': 'snippet',
-    'chart': 'mostPopular',
-    'key': api_key
-}
+    params = {
+        'part': 'snippet',
+        'chart': 'mostPopular',
+        'key': api_key
+    }
 
-response = requests.get(url, params=params)
-data = response.json()
-video_titles = [item['snippet']['title'] for item in data['items']]
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
+def extract_video_titles(data):
+    video_titles = [item['snippet']['title'] for item in data['items']]
+    return video_titles
+def save_video_titles_tod_database(video_titles, data_base_name):
+    data_frame = pd.DataFrame({'video_title': video_titles})
+    engine = db.create_engine('sqlite:///data_base_name.db')
+    data_frame.to_sql('table_name', con=engine, if_exists='replace', index=False)
+def retrieve_from_database(data_base_name):
+    engine = db.create_engine('sqlite:///data_base_name.db')
+    with engine.connect() as connection:
+        query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+        return pd.DataFrame(query_result)
 
-data_frame = pd.DataFrame({'video_title': video_titles})
-engine = db.create_engine('sqlite:///data_base_name.db')
-data_frame.to_sql('table_name', con=engine, if_exists='replace', index=False)
-
-with engine.connect() as connection:
-    query_result = connection.execute(
-        db.text("SELECT * FROM table_name;")
-    ).fetchall()
-    print(pd.DataFrame(query_result))
+data = makee_youtube_api_requests()
+video_titles = extract_video_titles(data)
+save_video_titles_tod_database(video_titles, 'data_base_name.db')
+retrieve_titles = retrieve_from_database('data_base_name.db')
+print(retrieve_titles)

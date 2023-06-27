b5bcacdcb782ff93a3fed80f886267660b156071
Yeabsira Tamiru <ytamiru@terpmail.umd.edu>
2023-06-27 16:25:40 +0000

    modify ytbe.py

--- ytbe.py
+++ ytbe.py
@@ -1,10 +1,8 @@
-#Makes a request to the YouTube Data API to 
-#retrieve the most popular videos. It then 
-#extracts the titles of those videos and prints them.
 import requests
 import pandas as pd
 import sqlalchemy as db
 import os
+
 
 def makee_youtube_api_requests():
     url = 'https://www.googleapis.com/youtube/v3/videos'
@@ -19,21 +17,36 @@ def makee_youtube_api_requests():
     response = requests.get(url, params=params)
     data = response.json()
     return data
+
+
 def extract_video_titles(data):
     video_titles = [item['snippet']['title'] for item in data['items']]
     return video_titles
-def save_video_titles_tod_database(video_titles, data_base_name):
+
+
+def save_video_titles_to_database(video_titles, data_base_name):
     data_frame = pd.DataFrame({'video_title': video_titles})
     engine = db.create_engine('sqlite:///data_base_name.db')
-    data_frame.to_sql('table_name', con=engine, if_exists='replace', index=False)
+    data_frame.to_sql(
+        'table_name',
+        con=engine,
+        if_exists='replace',
+        index=False
+    )
+
+
 def retrieve_from_database(data_base_name):
     engine = db.create_engine('sqlite:///data_base_name.db')
     with engine.connect() as connection:
-        query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+        query = "SELECT * FROM table_name;"
+        query_result = connection.execute(db.text(query)).fetchall()
         return pd.DataFrame(query_result)
+
 
 data = makee_youtube_api_requests()
 video_titles = extract_video_titles(data)
-save_video_titles_tod_database(video_titles, 'data_base_name.db')
+save_video_titles_to_database(video_titles, 'data_base_name.db')
+
+
 retrieve_titles = retrieve_from_database('data_base_name.db')
 print(retrieve_titles)
